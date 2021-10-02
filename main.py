d33415b998639159b93f545cf48a3ff05cc9c457
NguyenQuyPhuc20173302 <58975613+NguyenQuyPhuc20173302@users.noreply.github.com>
2021-10-02 10:24:03 +0700

    ok

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__=='__main__':
     Checkpoint = tf.keras.callbacks.ModelCheckpoint(
         filepath='model.hdf5',
         monitor='val_loss',
-        mode='min',)
+        save_best_only=True)
 
     transformer.compile(
         optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss="sparse_categorical_crossentropy",
